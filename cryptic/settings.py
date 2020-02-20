9ed67247b45b91947855d572ed8b4b3ec950bd64
alexiscorney <adcorney@googlemmail.com>
2020-02-20 17:35:42 +0000

    debug true

--- cryptic/settings.py
+++ cryptic/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dcj7j=2cp2b7f_+$hark)w@cj(bvi(0f$0qxq*80d0052%o3z%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'homepage.apps.HomepageConfig',
+    
 ]
 
 MIDDLEWARE = [
