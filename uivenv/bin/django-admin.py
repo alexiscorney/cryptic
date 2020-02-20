03676d873a3d5738128c9d3c8069efa9442fa95b
alexiscorney <adcorney@googlemmail.com>
2020-02-20 14:42:56 +0000

    set up git for django ui

---
+++ uivenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/adcorney/Desktop/FinalYearGit/axc680/cryptic/ui/uivenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
